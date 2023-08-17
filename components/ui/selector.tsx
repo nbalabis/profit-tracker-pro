c654ef7203837a7c4f5bbdd41c4f7606fd46f5cb
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-08-17 11:05:16 -1000

    Add inset shadow to selector component

--- components/ui/selector.tsx
+++ components/ui/selector.tsx
@@ -9,7 +9,7 @@ const Selector = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center gap-5 rounded-md bg-muted p-1 text-muted-foreground",
+      "inline-flex h-10 items-center justify-center gap-5 rounded-md bg-slate-200 p-1 text-muted-foreground shadow-inner",
       className,
     )}
     {...props}

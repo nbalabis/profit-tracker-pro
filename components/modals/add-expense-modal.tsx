7e9b143ada8f598742a5dce60f0f40afbfd586c8
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-09-16 22:23:46 -0700

    Add an expense form

--- components/modals/add-expense-modal.tsx
+++ components/modals/add-expense-modal.tsx
@@ -1,26 +1,215 @@
+"use client";
+
+import * as z from "zod";
+import { useState } from "react";
+import { format } from "date-fns";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { CalendarIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { useToast } from "@/components/ui/use-toast";
 import { useAddExpenseModal } from "@/hooks/use-add-expense-modal";
 
+// Create zod schema for form validation
+const formSchema = z.object({
+  title: z.string().nonempty({ message: "Expense title is required" }),
+  date: z.date({ required_error: "Date is required" }),
+  price: z.coerce.number().nonnegative({ message: "Price cannot be negative" }),
+});
+
 const AddExpenseModal = () => {
+  const router = useRouter();
+  const { toast } = useToast();
   const modal = useAddExpenseModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Define a form using react-hook-form and zod
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      date: new Date(),
+      price: 0,
+    },
+  });
+
+  // Submit handler
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      setIsLoading(true);
+
+      console.log(values);
+
+      form.reset();
+      modal.onClose();
+      router.refresh();
+      toast({
+        title: "Success!",
+        description: "Your expense has been added.",
+      });
+    } catch (error: any) {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: error?.response?.data || "Please try again later.",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <Dialog
       open={modal.isOpen}
       onOpenChange={() => {
         modal.onClose();
-        // form.reset();
+        form.reset();
       }}
     >
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add a New Expense</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div>
+              <div className="space-y-4 py-2 pb-4">
+                {/* Expense Information */}
+                <div className="space-y-2">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Expense</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="What was your expense?"
+                            disabled={isLoading}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="space-y-2">
+                  <div className="flex gap-2">
+                    <FormField
+                      control={form.control}
+                      name="price"
+                      render={({ field }) => (
+                        <FormItem className="w-1/2">
+                          <FormLabel>Price</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              type="number"
+                              step="0.01"
+                              placeholder="What did you pay?"
+                              disabled={isLoading}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="date"
+                      render={({ field }) => (
+                        <FormItem className="w-1/2">
+                          <FormLabel>Source Date</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  disabled={isLoading}
+                                  className={cn(
+                                    "w-[240px] pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground",
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() ||
+                                  date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit" disabled={isLoading}>
+                Add
+              </Button>
+              <Button
+                variant="outline"
+                type="button"
+                disabled={isLoading}
+                onClick={() => {
+                  modal.onClose();
+                  form.reset();
+                }}
+              >
+                Cancel
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
